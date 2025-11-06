75181833ea18bea63885b1796d3ed08d4d7f20f6
ansimonn <ansimonn@student.42angouleme.fr>
2025-11-06 18:10:11 +0100

    files created

---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ansimonn <ansimonn@student.42angouleme.f>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/06 15:24:58 by ansimonn          #+#    #+#             */
+/*   Updated: 2025/11/06 15:40:39 by ansimonn         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GNL_GET_NEXT_LINE_H
+# define GNL_GET_NEXT_LINE_H
+
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+
+#endif //GNL_GET_NEXT_LINE_H
